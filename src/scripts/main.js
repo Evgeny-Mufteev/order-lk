02969de74e581de312fd656430db7dcf7d54be42
Mufteev Evgeny <evgenymufteev@gmail.com>
2023-08-08 17:48:52 +0300

    add layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -26,4 +26,75 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   };
   handleTabs('.tabs', '.tabs__btn', '.tabs__item', 'is-active');
+
+  // Редактирование инпутов при клике на иконки
+  const enableInputEditingOnIconClick = () => {
+    const arrIconEdit = document.querySelectorAll('.js-icon');
+
+    arrIconEdit.forEach((iconEdit) => {
+      iconEdit.addEventListener('click', () => {
+        const input = iconEdit.closest('.js-editing').querySelector('.js-editing-input');
+
+        input.removeAttribute('readonly');
+        input.focus();
+
+        const clickOutsideHandler = (event) => {
+          if (!input.contains(event.target)) {
+            input.setAttribute('readonly', 'true');
+            document.removeEventListener('click', clickOutsideHandler);
+          }
+        };
+
+        setTimeout(() => {
+          document.addEventListener('click', clickOutsideHandler);
+        }, 0);
+      });
+    });
+  };
+  enableInputEditingOnIconClick();
+
+  // Маска телефона
+  const handlePhoneMask = (input) => {
+    let matrix = '+7 (___) ___-__-__',
+      i = 0,
+      def = matrix.replace(/\D/g, ''),
+      val = input.value.replace(/\D/g, '');
+    if (def.length >= val.length) val = def;
+    input.value = matrix.replace(/./g, function (a) {
+      return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
+    });
+  };
+  const numbers = document.querySelectorAll('input[type="tel"]');
+  numbers.forEach((number) => {
+    number.addEventListener('input', handlePhoneMask.bind(null, number));
+    number.addEventListener('focus', handlePhoneMask.bind(null, number));
+    number.addEventListener('blur', handlePhoneMask.bind(null, number));
+  });
+
+  // Проверка на совпадение паролей
+  const newPasswordInput = document.querySelector('.js-new-password');
+  const confirmPasswordInput = document.querySelector('.js-confirm-password');
+  const infoMessage = document.querySelector('.js-message');
+
+  const checkPasswords = () => {
+    const newPassword = newPasswordInput.value;
+    const confirmPassword = confirmPasswordInput.value;
+
+    if (newPassword.length >= 6 && confirmPassword.length >= 6) {
+      if (newPassword !== confirmPassword) {
+        infoMessage.textContent = 'Пароли не совпадают';
+      } else {
+        infoMessage.textContent = 'Пароли совпадают';
+      }
+    } else {
+      infoMessage.textContent = 'Пароль должен содержать не менее 6 символов';
+    }
+  };
+
+  newPasswordInput.addEventListener('input', checkPasswords);
+  confirmPasswordInput.addEventListener('input', checkPasswords);
+
+  // ghkhujnkjhik
+
+  // const arrIcon = document.q
 });
