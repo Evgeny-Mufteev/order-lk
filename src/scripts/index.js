94d5c8ca9f3094528d0842fb58ba7a85d71adf46
Eugene-Maikov <eugene-maikov2000@yandex.ru>
2023-08-31 11:25:44 +0300

    feat: add notes counter

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,7 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', () => {
 
-  const counter = (listElements, showCounter) => {
+  const counterMassage = (listElements, showCounter) => {
     const list = document.querySelectorAll(listElements)
     const show = document.querySelector(showCounter)
 
@@ -18,16 +18,36 @@ document.addEventListener('DOMContentLoaded', () => {
       if (!list[i].classList.contains('messages__item--read')) {
         list[i].addEventListener('click', () => {
           if (!list[i].classList.contains('messages__item--read')) {
-            list[i].classList.add('messages__item--read');
+            list[i].classList.add('messages__item--read')
             count--;
-            show.innerHTML = count;
+            show.innerHTML = count
           }
-        });
+        })
       }
     }
   }
+  const counterNotes = (listElements, showCounter) => {
+    const list = document.querySelectorAll(listElements)
+    const show = document.querySelector(showCounter)
 
-  counter('.message__list-item', '.message__counter')
-  counter('.notes__list-item', '.notes__counter')
+    let count = 0
+
+    for (let i = 0; i < list.length; i++) {
+      count++
+    }
+    show.innerHTML = count
+
+    for (let i = 0; i < list.length; i++) {
+      const del = list[i].querySelector('.js-delete-btn')
+
+      del.addEventListener('click', () => {
+        count--
+        show.innerHTML = count
+      })
+    }
+  }
+
+  counterMassage('.message__list-item', '.message__counter')
+  counterNotes('.notes__list-item', '.notes__counter')
 
 });
